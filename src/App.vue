c31b7374d540b3f3b56c440b1934fd9450944863
Nikita <nikitavidnov@yandex.ru>
2024-11-12 00:50:20 +0300

    main - create component

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <main>
+    <h1>Select with Search</h1>
+    <SelectWithSearch
+        :options="options"
+        v-model="selectedOption"
+    />
+  </main>
+</template>
+
+<script>
+import SelectWithSearch from './components/vSelect.vue';
+
+export default {
+  components: {
+    SelectWithSearch,
+  },
+  data() {
+    return {
+      options: [
+        { id: Symbol(1), name: 'Option 1' },
+        { id: Symbol(1), name: 'Option 2' },
+        { id:Symbol(1), name: 'Option 3' },
+        { id: Symbol(1), name: 'Option 4' },
+      ],
+      selectedOption: {id: 1,name: 'Option 1'},
+    };
+  },
+};
+</script>
+<style lang="scss">
+main {
+  width: 100%;
+}
+</style>
---
+++ src/components/vSelect.vue
@@ -0,0 +1,177 @@
+<template>
+  <div id="select-with-search" class="select-with-search">
+    <div class="select-control" @click="handleShowDropdown">
+      <div class="select-control__title">
+        <span class="title">Номер телефона</span>
+      </div>
+      <div class="select-control__input">
+        <input v-model="searchTerm" @input="filterOptions"  class="input" type="text">
+      </div>
+    </div>
+    <transition name="dropdown-fade">
+      <div class="dropdown" v-show="showDropdown">
+        <div class="dropdown-wrapper" v-if="filteredOptions.length">
+          <ul class="lists">
+            <li class="list"  v-for="list in filteredOptions" :key="list.id" @click="handleOptionSelect(list)">{{list.name}}</li>
+          </ul>
+        </div>
+        <div v-else class="dropdown-wrapper">
+          <span> Нет данных</span>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    modelValue: {
+      type: [String, Object],
+    }
+  },
+  data() {
+    return {
+      showDropdown: false,
+      searchTerm: '',
+      filteredOptions: this.options,
+    };
+  },
+  created() {
+    if (this.modelValue) {
+      if(typeof this.modelValue === 'string'){
+        this.searchTerm = this.modelValue;
+      }else {
+        this.searchTerm = this.modelValue.name;
+      }
+    }
+  },
+  mounted() {
+    document.addEventListener('click', this.handleClick)
+  },
+  destroyed() {
+    document.removeEventListener('click', this.handleClick)
+  },
+  methods: {
+    handleShowDropdown(){
+      this.showDropdown = !this.showDropdown;
+    },
+    handleClick(event) {
+      const element = document.getElementById('select-with-search');
+      if (!element.contains(event.target)) {
+        this.showDropdown = false;
+      }
+},
+    handleOptionSelect(option) {
+      this.$emit('update:model-value', option); // Измените на option, чтобы возвращать весь объект
+      this.searchTerm = option.name; // Или любое другое поле, которое хотите отобразить
+      this.showDropdown = false;
+      this.filteredOptions = this.options; // Сброс фильтрации
+    },
+    filterOptions() {
+      this.filteredOptions = this.options.filter(option =>
+          option.name.toLowerCase().includes(this.searchTerm.toLowerCase()) // Или другое поле
+      );
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.select-with-search {
+  display: flex;
+  position: relative;
+  font-family: Roboto, sans-serif;
+  background: #ffffff;
+  border: 1px solid #E4E6E7;
+  border-radius: 3px;
+  .select-control {
+    display: flex;
+    flex-flow: column;
+    gap: 4px;
+    padding: 9px 16px 4px;
+    width: 100%;
+    &__title {
+      .title {
+        color: #AAAAAA;
+        font-size: 11px;
+        line-height: 14px;
+        font-weight: 400;
+      }
+    }
+    &__input {
+      .input {
+        border: none;
+        color: #2b2b2b;
+        font-size: 16px;
+        line-height: 20px;
+        font-weight: 400;
+        width: 100%;
+        &:focus-visible {
+          border: none;
+          outline: none;
+        }
+      }
+    }
+  }
+  .dropdown {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    padding: 12px;
+    border: 1px solid #E4E6E7;
+    border-radius: 9px;
+    background: #ffffff;
+    box-shadow: 0 4px 16px 0 #D1D1D140;
+    .dropdown-wrapper {
+      max-height: 260px;
+      overflow: scroll;
+      .lists {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-flow: column;
+        gap: 4px;
+        .list {
+          cursor: pointer;
+          padding: 8px 16px;
+          font-family: Roboto, sans-serif;
+          font-size: 14px;
+          font-weight: 400;
+          line-height: 20px;
+          text-align: left;
+          list-style: none;
+          border-radius: 3px;
+          &:hover {
+            background: #F7F9FB;
+            color: #48538B;
+          }
+        }
+        .selected {
+          background: #48538B;
+          color: #ffffff;
+          &:hover {
+            background: #48538B;
+            color: #ffffff;
+          }
+        }
+      }
+    }
+  }
+}
+
+///* Переходы для dropdown */
+.dropdown-fade-enter-active,
+.dropdown-fade-leave-active {
+  transition: opacity .3s ease-in;
+}
+
+.dropdown-fade-enter-from,
+.dropdown-fade-leave-to {
+  opacity: 0;
+}
+</style>
